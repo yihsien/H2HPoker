db3628063e53a82fa1def5662695c8e01c9c9e04
Stephen Lin <yihsien@princeton.edu>
2014-05-04 17:40:18 -0400

    class file for bet

---
+++ src/lib/Bet.java
@@ -0,0 +1,22 @@
+package lib;
+
+import interfaces.IBet;
+
+public class Bet implements IBet {
+
+	private int value = 0;
+	public static final int FOLD = -2;
+	public static final int CHECK = -1;
+	public static final int CALL = 0;
+	
+	@Override
+	public void updateBet(int amount) {
+		value = amount;
+	}
+
+	@Override
+	public int getBet() {
+		return value;
+	}
+
+}
