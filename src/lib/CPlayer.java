4d26d5d1325c248408db661aabfeebd243b144cc
Stephen Lin <yihsien@princeton.edu>
2014-05-04 19:43:39 -0400

    added CPlayer

--- src/lib/CPlayer.java
+++ src/lib/CPlayer.java
@@ -7,23 +7,55 @@ import java.util.*;
 public class CPlayer implements IPlayer {
 
 	private int money;
+	private ArrayList<ICard> hand = new ArrayList<ICard>();
 	
 	@Override
 	public ArrayList<ICard> getHand() {
-		// TODO Auto-generated method stub
-		return null;
+		return hand;
 	}
 
 	@Override
 	public int getMoney() {
-		// TODO Auto-generated method stub
-		return 0;
+		return money;
 	}
 
 	@Override
 	public int getBet(int bet) {
-		// TODO Auto-generated method stub
-		return 0;
+		int finalBet;
+		Random rand = new Random();
+		while(true){
+			int playerBet = rand.nextInt(5)+1;;
+			if(playerBet == 1){
+				finalBet = Bet.FOLD;
+				break;
+			}
+			else if(playerBet == 2){
+				if(bet!=0){
+					continue;
+				}
+				finalBet = Bet.CHECK;
+				break;
+			}
+			else if(playerBet == 3){
+				if(money<bet){
+					continue;
+				}
+				finalBet = Bet.CALL;
+				break;
+			}
+			else if(playerBet == 4){
+				finalBet = Bet.ALLIN;
+				break;
+			}
+			else{
+				if((money-bet)<bet){
+					continue;
+				}
+				finalBet = rand.nextInt(money-bet);
+				break;
+			}
+		}
+		return finalBet;
 	}
 	
 	public void addMoney(int amount) { 
@@ -36,7 +68,7 @@ public class CPlayer implements IPlayer {
 
     @Override
     public void addCard(ICard card) {
-        
+    	hand.add(card);
     }
 
 }
